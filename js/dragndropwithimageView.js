72c1e5bd7d1478d5c93f6afdc00b4bfe81e4c827
tueho <tue.ho@edugix.com>
2025-07-14 15:16:50 +0700

    fix

--- js/dragndropwithimageView.js
+++ js/dragndropwithimageView.js
@@ -421,7 +421,7 @@ define([
           this.$('.dragndropwi-question'),
           function (question, index) {
             const $container = $(
-              '<div class="user-answer" style="padding: 10px; display: block; position: absolute; top: 0; left: 0; width: 100%; display: flex; gap: 10px; flex-wrap: wrap; z-index: 20;"></div>'
+              '<div class="user-answer" style="padding: 10px; display: block; position: absolute; top: 43px; left: 23px; width: 100%; display: flex; gap: 10px; flex-wrap: wrap; z-index: 20;"></div>'
             );
             const imageSources = items?.[parseInt(index)]?._userAnswer?.filter(item => !!item)?.reverse() || [];
 
